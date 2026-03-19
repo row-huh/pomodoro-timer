7ad82e8747000871f60e08a2331d6d14c3d4c39b
Roha Pathan <rohaabropathan@gmail.com>
2026-03-19 23:37:00 +0500

    guah

--- pomodoro-timer/app/page.tsx
+++ pomodoro-timer/app/page.tsx
@@ -1,6 +1,28 @@
+"use client"
+
+import { useState } from "react";
+
+
+function addTask(task: string, tasks: string[]) {
+  
+}
+
 export default function Home() {
+  // usestate only stores while running (memory stays for the session)
+  const [tasks, setTasks] = useState<string[]>([]);
+
+
   return (
-    <div></div>
+    <div>
+      <form>
+        <label htmlFor="tasks"></label>
+        <input type="text" />
+        <input type="submit" onClick={addTask()} />
+      </form>
+    </div>
+    // add a button + text field
+    // clickin on button submits form
+    // anything inside it appends to tasks usestate?
 );
 }
 
